78e38ca53b5760bdaedf158a71dcc95086fa70bd
Lucas-850 <lucasrodriguesocp@gmail.com>
2025-06-26 14:31:57 -0300

    quickstart urlfor

--- quick_start.py
+++ quick_start.py
@@ -7,11 +7,29 @@ def hello_word():
     return "index page"
 
 @app.route("/hello")
-def hello():
-    return f"Hello!"
+def hello(name="Lucas"):
+    return f"Hello, World!"
 
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'user {escape(username)}'
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f"Post {post_id}"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f"Subpath: {escape(subpath)}"
+
+@app.route("/projects/")
+def projects():
+    return "The project page"
+
+@app.route("/about")
+def about():
+    return "The about page" 
 
 
 if __name__ == '__main__':
