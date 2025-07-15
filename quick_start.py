8b7e69bcc590d9c0a8af51549645dd273d9bbee9
Lucas-850 <lucasrodriguesocp@gmail.com>
2025-07-15 16:00:37 -0300

    methodos https

--- quick_start.py
+++ quick_start.py
@@ -1,10 +1,16 @@
-from flask import Flask 
+from flask import Flask , url_for, request
 from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/')
-def hello_word():
+def index():
     return "index page"
+
+@app.route('/login', methods= ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    return "Login Page"
 
 @app.route("/hello")
 def hello(name="Lucas"):
@@ -12,8 +18,8 @@ def hello(name="Lucas"):
 
 
 @app.route('/user/<username>')
-def show_user_profile(username):
-    return f'user {escape(username)}'
+def profile(username):
+    return f"{username}\'s profile "
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -30,6 +36,16 @@ def projects():
 @app.route("/about")
 def about():
     return "The about page" 
+
+
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
 
 
 if __name__ == '__main__':
